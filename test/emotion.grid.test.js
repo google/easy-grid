fc529060cec597fff77e56cce3269bcccd7086fa
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-02-26 11:15:24 -0500

    Update tests and add emotion tests

--- test/emotion.grid.test.js
+++ test/emotion.grid.test.js
@@ -1,23 +1,9 @@
-/**
- * Copyright 2017 Google LLC
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * https://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import grid from '../src/grid.js';
-import 'jest-styled-components'
+import grid from '../emotion';
 import React from 'react';
 import { create as render } from 'react-test-renderer'
+import serializer, {matchers } from 'jest-emotion'
+expect.extend(matchers)
+expect.addSnapshotSerializer(serializer)
 
 const renderJSON = el => render(el).toJSON()
 
@@ -104,10 +90,11 @@ describe('The grid component should', () => {
 			<div></div>
 		</Component>)
 
-		expect(json).toMatchSnapshot()
+        expect(json).toMatchSnapshot()
+        //console.log(JSON.stringify(json))
 		const firstGridItem = json.children[0]
 		const secondGridItem = json.children[1]
-
+      
 		expect(firstGridItem).toHaveStyleRule('grid-row-start', '1');
 		expect(firstGridItem).toHaveStyleRule('grid-row-end', '3');
 		expect(firstGridItem).toHaveStyleRule('grid-column-start', '1');
